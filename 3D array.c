8d823499aca85f24b349b1d50ddc93f796b5ef4c
Owen634 <muthomiowen6@gmail.com>
2024-10-18 00:01:44 +0300

    Add files via upload

---
+++ 3D array.c
@@ -0,0 +1,19 @@
+//program to calculate 3D array
+/*
+Author:Murithi Owen Muthomi
+Reg no:CT101/G/23216/24
+Date: 16/10/2024
+*/
+//preprocessor directive-printf(),scanf()
+
+#include<stdio.h>
+int main(){
+	int i,j,k;
+	  int marks[2][2][3]={ {40,50,60},{80,30,92},{39,20,15} };
+	  for(i=0;i<2;i++){
+	      for(k=0;k<3;k++){
+	  printf("marks[%d][%d][%d]=%d \n",i,j,k marks[i][j][k]);
+ }
+	  }}
+return 0;
+}
