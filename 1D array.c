7090f9e02fe30257eee12103810b39161709664c
Owen634 <muthomiowen6@gmail.com>
2024-10-17 21:22:19 +0300

    Add files via upload

---
+++ 1D array.c
@@ -0,0 +1,17 @@
+//program to calculate 1D array
+/*
+Author:Murithi Owen Muthomi
+Reg no:CT101/G/23216/24
+Date:17/10/2024
+*/
+//preprocessor directive-printf(),scanf()
+
+#include<stdio.h>
+int main(){
+	  int marks[5]= {50,60,70,80,2};
+	  int i;
+	  for(i=0;i<5;i++){
+	  printf("marks[%d]=%d \n",i,marks[i]);
+ }
+return 0;
+}
